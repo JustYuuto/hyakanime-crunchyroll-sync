6162b2148de73563e5e3e8cf156acdeeaa06733e
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2023-08-09 00:27:19 +0200

    script

--- js/watchlist-sync.js
+++
@@ -1,5 +0,0 @@
-function getAnimesNames() {
-  return Array.from(document
-    .querySelectorAll('.text--gq6o-.text--is-semibold--AHOYN.text--is-l--iccTo.watchlist-card-title__text--chGlt'))
-    .map(name => name.textContent);
-}
